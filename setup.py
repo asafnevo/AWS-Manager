5b50a4df9279121440f97aae1ebdf65e924c849e
Asaf Nevo <nevo@pico-app.com>
2015-12-21 15:18:44 +0200

    changed project short description

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = '0.2'
-description = "Manage your AWS resources easily"
+description = "An open source project for managing your AWS resources easily in your day to day coding"
 
 setup(
       name="AWS Manager",
       version=version,
       description=description,
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      install_requires=["boto3"],
       entry_points={
         'console_scripts': [
             'awsmanager = aws_manager.main:main',
