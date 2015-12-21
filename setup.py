04657e95d304f61b19013a39e92badb6d875179d
Asaf Nevo <nevo@pico-app.com>
2015-12-21 15:01:09 +0200

    started working on v0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 description = "Manage your AWS resources easily"
 
 setup(
       name="AWS Manager",
+      version=version,
       description=description,
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       entry_points={
