166f98f18f9e4b90ac5214513cebe47fda3ee4b0
Asaf Nevo <nevo@pico-app.com>
2015-12-21 16:40:11 +0200

    added setup.py variables

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ description = "An open source project for managing your AWS resources easily in 
 long_description = "README.md"
 author = 'Asaf Nevo, Aviv Paz',
 author_email = 'asafnevo1@gmail.com, avivpaz43556@gmail.com'
+license_type = "GNU"
+keywords = 'aws ec2 amazon servers vpc awscli'
 
 
 def read(fname):
@@ -17,6 +19,7 @@ setup(
         author_email=author_email,
         name="AWS Manager",
         version=version,
+        license=license_type,
         description=description,
         long_description=read(long_description),
         packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
@@ -26,6 +29,7 @@ setup(
                 'awsmanager = aws_manager.main:main',
             ],
         },
+        keywords=keywords,
         zip_safe=False,
         include_package_data=True
 )
